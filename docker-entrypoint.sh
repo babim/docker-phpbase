b3c6752c3ef347d51a2855e4fd29bc250e64de64
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-12-11 02:55:51 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,17 +6,45 @@ then
 	cp -R /etc-start/php/conf.d/* /usr/local/etc/php/conf.d
 fi
 
+
 # set ID docker run
 agid=${agid:-$auid}
-auser=www-data
+auser=${auser:-www-data}
 
 if [[ -z "${auid}" ]]; then
   echo "start"
 elif [[ "$auid" = "0" ]] || [[ "$aguid" == "0" ]]; then
- echo "can't run in Root user. Default user still run."
+	echo "run in user root"
+	auser=root
+	#export APACHE_RUN_USER=$auser
+	#export APACHE_RUN_GROUP=$auser
+	sed -i -e "/^user = .*/cuser = $auser" /usr/local/etc/php-fpm.conf
+	sed -i -e "/^group = .*/cgroup = $auser" /usr/local/etc/php-fpm.conf
 else
-  usermod -u $auid $auser
-  groupmod -g $agid $auser
+if id $auser >/dev/null 2>&1; then
+        echo "user exists"
+	sed -i -e "/^user = .*/cuser = $auser" /usr/local/etc/php-fpm.conf
+	sed -i -e "/^group = .*/cgroup = $auser" /usr/local/etc/php-fpm.conf
+	#export APACHE_RUN_USER=$auser
+	#export APACHE_RUN_GROUP=$auser
+	# usermod alpine
+		#deluser $auser && delgroup $auser
+		#addgroup -g $agid $auser && adduser -D -H -G $auser -s /bin/false -u $auid $auser
+	# usermod ubuntu/debian
+		usermod -u $auid $auser
+		groupmod -g $agid $auser
+else
+        echo "user does not exist"
+	#export APACHE_RUN_USER=$auser
+	#export APACHE_RUN_GROUP=$auser
+	# create user alpine
+	#addgroup -g $agid $auser && adduser -D -H -G $auser -s /bin/false -u $auid $auser
+	# create user ubuntu/debian
+	groupadd -g $agid $auser && useradd --system --uid $auid --shell /usr/sbin/nologin -g $auser $auser
+	sed -i -e "/^user = .*/cuser = $auser" /usr/local/etc/php-fpm.conf
+	sed -i -e "/^group = .*/cgroup = $auser" /usr/local/etc/php-fpm.conf
+fi
+
 fi
 
 exec "$@"
