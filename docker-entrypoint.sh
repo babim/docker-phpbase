d13875900403c1701b47f82f29318fe32dee4910
Duc Anh Babim <ducanh.babim@yahoo.com>
2017-02-05 21:39:33 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,24 @@ set -e
 if [ -z "`ls /usr/local/etc/php/conf.d`" ] 
 then
 	cp -R /etc-start/php/conf.d/* /usr/local/etc/php/conf.d
+    # Set environments
+    TIMEZONE1=${TIMEZONE:-Asia/Ho_Chi_Minh}
+    PHP_MEMORY_LIMIT1=${PHP_MEMORY_LIMIT:-512M}
+    MAX_UPLOAD1=${MAX_UPLOAD:-520M}
+    PHP_MAX_FILE_UPLOAD1=${PHP_MAX_FILE_UPLOAD:-200}
+    PHP_MAX_POST1=${PHP_MAX_POST:-520M}
+    MAX_INPUT_TIME1=${MAX_INPUT_TIME:-3600}
+    MAX_EXECUTION_TIME1=${MAX_EXECUTION_TIME:-3600}
+	
+	sed -i -E \
+	-e "s|;*date.timezone =.*|date.timezone = ${TIMEZONE1}|i" \
+	-e "s|;*memory_limit =.*|memory_limit = ${MAX_UPLOAD1}|i" \
+ 	-e "s|;*upload_max_filesize =.*|upload_max_filesize = ${MAX_UPLOAD1}|i" \
+    	-e "s|;*max_file_uploads =.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD1}|i" \
+    	-e "s|;*post_max_size =.*|post_max_size = ${PHP_MAX_POST1}|i" \
+    	-e "s/max_input_time = 60/max_input_time = ${MAX_INPUT_TIME1}/" \
+	-e "s/max_execution_time = 30/max_execution_time = ${MAX_EXECUTION_TIME1}/" \
+	/usr/local/etc/php/conf.d/babim.ini
 fi
 
 
